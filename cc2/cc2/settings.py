527e5979eb67702c18af78c24da95be92115fa7c
Sanchit Pruthi <sanchitpruthi@gmail.com>
2020-05-07 23:20:39 -0700

    More Forms Updated

--- cc2/cc2/settings.py
+++ cc2/cc2/settings.py
@@ -103,6 +103,12 @@ else:
             'PASSWORD': 'admin',
         }
     }
+    # DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #     }
+    # }
 # [END db_setup]
 
 
